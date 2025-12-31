f9e3fdba1b5a37a55c3dafc42cdb6bc9e8f5d9cb
Yuto Ichinose <60034520+illionillion@users.noreply.github.com>
2025-12-31 06:44:33 +0000

    test(debug): CIデバッグのためにモックデータ変更

--- frontend/features/posts/components/Timeline/Timeline.stories.tsx
+++ frontend/features/posts/components/Timeline/Timeline.stories.tsx
@@ -13,7 +13,7 @@ const mockPosts: Post[] = [
   {
     id: 1,
     user_id: 1,
-    message: "今日のシーシャは最高でした！ミント系のフレーバーが爽やかで最高",
+    message: "今日のシーシャは最高でした！ミント系のフレーバーが爽やかで最高！！！！！！！！！hoge",
     slides: [
       {
         image_url: "https://placehold.co/400x600/CCCCCC/666666?text=Mint",
